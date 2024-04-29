dc21fc6db813c196d315545e5caa3fa428a7fab1
Lennon Daniel <lennon.v90@gmail.com>
2024-04-29 08:58:34 -0300

    feat: add loading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import PokeAPI, { IPokemon } from 'pokeapi-typescript'
+import Loading from './components/Loading'
 
 function App() {
   const [pokemon, setPokemon] = useState<IPokemon | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
-    PokeAPI.Pokemon.resolve("pikachu").then(result => setPokemon(result));
+    PokeAPI.Pokemon.resolve("pikachu").then(result => {
+      setLoading(false)
+      setPokemon(result)
+    });
   }, [])
 
   return (
     <>
-     <pre>
-        { pokemon?.name }
-     </pre>
+     <Loading isLoading={loading} />
     </>
   )
 }
